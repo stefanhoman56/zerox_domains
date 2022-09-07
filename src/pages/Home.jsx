717a7ca49591e26b2941da5516a8332b84521b85
snape1112 <snape793@gmail.com>
2022-09-07 08:30:08 +0200

    initial project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import LOGO from '../assets/images/logo.png';
+
+const Home = () => {
+    return (
+        <>
+            <img id="logo" src={LOGO}/>
+        </>
+    )
+};
+
+export default Home;
---
+++ src/routes/index.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+// import axios from 'axios';
+import { Route, Routes } from 'react-router-dom';
+import Home from '../pages/Home';
+import About from '../pages/About';
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/aboutus" element={<About />} />
+    </Routes>
+  );
+};
+
+export default Router;
